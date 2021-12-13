15a44e0db9846117d7002db20332fd593132f775
有虫快跑 <798998599@qq.com>
2021-12-13 16:05:52 +0800

    对redis中key的作用注释

--- src/main/java/com/ruyuan/little/project/rocketmq/common/constants/RedisKeyConstant.java
+++ src/main/java/com/ruyuan/little/project/rocketmq/common/constants/RedisKeyConstant.java
@@ -8,27 +8,27 @@ package com.ruyuan.little.project.rocketmq.common.constants;
 public class RedisKeyConstant {
 
     /**
-     * 酒店房间key的前缀
+     *【缓存】酒店房间key的前缀
      */
     public static final String HOTEL_ROOM_KEY_PREFIX = "little:project:hotelRoom:";
 
     /**
-     * 订单重复支付
-     */
-    public static final String ORDER_DUPLICATION_KEY_PREFIX = "little:project:payOrderDuplication:";
-
-    /**
-     * 订单支付和取消分布式锁key
+     *【分布式锁】订单支付和取消分布式锁key
      */
     public static final String ORDER_LOCK_KEY_PREFIX = "little:project:orderLock:";
 
     /**
-     * 第一次登陆重复消费 保证幂等的key前缀
+     *【幂等】第一次登陆重复消费 保证幂等的key前缀
      */
     public static final String FIRST_LOGIN_DUPLICATION_KEY_PREFIX = "little:project:firstLoginDuplication:";
 
     /**
-     * 完成订单后重复消费 保证幂等key前缀
+     *【幂等】订单重复支付
+     */
+    public static final String ORDER_DUPLICATION_KEY_PREFIX = "little:project:payOrderDuplication:";
+
+    /**
+     *【幂等】完成订单后重复消费 保证幂等key前缀
      */
     public static final String ORDER_FINISHED_DUPLICATION_KEY_PREFIX = "little:project:orderFinishedDuplication:";
 
